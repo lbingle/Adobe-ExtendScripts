9d82478ce8226eda3abeabd6d6bd9b1cee5a7b4f
lbingle <lebingle@gmail.com>
2017-09-04 14:26:51 -0700

    Revert "Inital Files"
    This reverts commit b6981f944e659358416ca5e7d89311a22f36c38c.

--- Export2.jsx
+++
@@ -1,4 +0,0 @@
-﻿//exports active document to a PDF, simplifies script with added variable, still need to figure out how to refer to pages in the document
-var Rfile = new File('C:/Users/lbingle/Desktop/test.pdf');
-var myDoc = app.activeDocument;
-myDoc.exportFile(ExportFormat.PDF_TYPE, Rfile);
--- test.jsx
+++
@@ -1,6 +0,0 @@
-﻿for (var i = 0; i = allPage.length; i++) {
-var page = allPage[i];
-if (i < firstPage || i > lastPage) {
-page.remove();
-}
-};
